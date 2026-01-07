a4b21af4b017f86c5476b81df4b7a13c50cf75a8
filip <filip.trikov.iliev@zohomail.eu>
2026-01-07 23:54:23 +0100

    Fix ignorePatterns and move notes to content root

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "filip-tripkov-iliev.github.io/FTI-digital-garden",
-    ignorePatterns: ["private", "templates", ".obsidian", "**/*", "!content/published/**"],
+    ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
